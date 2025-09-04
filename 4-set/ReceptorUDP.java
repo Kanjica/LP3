5836159d53b4c5f878dc2913d8d8266285331150
Kanjica <joaopedro24960@gmail.com>
2025-09-04 10:28:44 -0300

    4-set

---
+++ 4-set/ReceptorUDP.java
@@ -0,0 +1,25 @@
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import javax.swing.JOptionPane;
+
+public class ReceptorUDP {
+    public static void main(String[] args) {
+        if(args.length != 1){
+            System.out.println("Informe a porta a ser ouvida");
+            System.exit(0);
+        }
+        try {
+            int port = Integer.parseInt(args[0]);
+            DatagramSocket ds = new DatagramSocket(port);
+            byte[] msg = new byte[256];
+            DatagramPacket pkg = new DatagramPacket(msg, msg.length);
+            ds.receive(pkg);
+
+            JOptionPane.showMessageDialog(null,new String(pkg.getData()).trim(), "Mensagem recebida", 1);
+            ds.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
---
+++ 4-set/RemetenteUDP.java
@@ -0,0 +1,26 @@
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class RemetenteUDP{
+    public static void main(String[] args) {
+        
+        if(args.length != 3){
+            System.out.println("Uso correto: <Nome da maquina> <Porta> <Mensagem>");
+            System.exit(0);
+        }
+        try {
+            InetAddress addr = InetAddress.getByName(args[0]);
+            int port = Integer.parseInt(args[1]);
+            byte[] msg = args[2].getBytes();
+            DatagramPacket pkg = new DatagramPacket(msg,msg.length,addr, port);
+            DatagramSocket ds = new DatagramSocket();
+            ds.send(pkg);
+            System.out.println("Mensagem enviada para: " + addr.getHostAddress() + "\n"+ "Porta: " + port + "\n" + "Mensagem: " + args[2]);
+            ds.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+}
