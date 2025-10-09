ae5e3e7a7c155f89faaa2dc38738e23f60d28ed3
Kanjica <joaopedro24960@gmail.com>
2025-10-08 21:56:58 -0300

    adicionando DTO

--- exercicio2/Estacionamento.java
+++ exercicio2/Estacionamento.java
@@ -1,5 +1,7 @@
 package exercicio2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,24 +14,40 @@ public class Estacionamento {
         Semaforos semaforos = new Semaforos(new Semaphore(2), new Semaphore(5), 
                             new Semaphore(1), new Semaphore(1));
 
+        List<Integer> tempoEstacionado = new ArrayList<>();
+
         ExecutorService executor = Executors.newFixedThreadPool(4);
         int veiculosQueNaoEstacionaram = 0;
         int veiculosQueEstacionaram = 0;
 
+        List<Future<veiculoDTO>> resultadosPendentes = new ArrayList<>(); 
+
         for(int i=0; i<20; i++){
             double probabilidade = Math.random();
-
-            Future<Boolean> estacionou = executor.submit(new Veiculo(("Carro " + i), 
+            Future<veiculoDTO> veiculoDados = executor.submit(new Veiculo(("Carro " + i), 
                                             probabilidade<=0.30? false : true, 
                                             semaforos));
-
-            if(!estacionou.get()){veiculosQueNaoEstacionaram ++;}
-            else{veiculosQueEstacionaram++;}
-
-            //System.out.println("Veiculos que estacionaram: "+veiculosQueEstacionaram);
-            //System.out.println("Veiculos que nao estacionaram: "+veiculosQueNaoEstacionaram);
+            resultadosPendentes.add(veiculoDados); 
         }
 
+        for (Future<veiculoDTO> future : resultadosPendentes) {
+            veiculoDTO dados = future.get(); 
+            
+            if(!dados.estacionou()){
+                veiculosQueNaoEstacionaram++;
+            } 
+            else{
+                veiculosQueEstacionaram++;
+                tempoEstacionado.add(dados.tempoEstacionado());
+            }
+        }
 
+        executor.shutdown(); 
+
+        double tempoMedio = tempoEstacionado.stream()
+                                .mapToInt(Integer::intValue)
+                                .sum();
+
+        System.out.println("Tempo médio: " + tempoMedio/tempoEstacionado.size());
     }
 }
--- exercicio2/Veiculo.java
+++ exercicio2/Veiculo.java
@@ -5,22 +5,25 @@ import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Veiculo implements Callable<Boolean>{
+public class Veiculo implements Callable<veiculoDTO>{
     private static final Logger logger = Logger.getLogger(Veiculo.class.getName());
+
     public String name;
     public boolean isPrioridade;
     public Semaforos semaforos;
     public boolean estacionou;
+    public int tempoEstacionado;
 
     public Veiculo(String name, boolean isPrioridade, Semaforos semaforos){
         this.name = name;
         this.isPrioridade = isPrioridade;
         this.semaforos = semaforos;
         this.estacionou = false;
+        this.tempoEstacionado = 0;
     }
 
     @Override
-    public Boolean call() throws Exception{
+    public veiculoDTO call() throws Exception{
         Random random = new Random();
         try{
             semaforos.portaoEntrada().acquire();
@@ -33,7 +36,7 @@ public class Veiculo implements Callable<Boolean>{
                 if(semaforos.vagaPrioritaria().tryAcquire()){
                     estacionou = true;
                     logger.log(Level.INFO, "Veículo {0} estacionou na vaga prioritária.", name);
-                    Thread.sleep(random.nextInt(1000, 5000)); 
+                    Thread.sleep(tempoEstacionado = random.nextInt(1000, 5000)); 
                     semaforos.vagaPrioritaria().release();
                 }else{
                     logger.log(Level.WARNING, "Veículo {0} não conseguiu estacionar na vaga prioritária.", name);
@@ -45,26 +48,27 @@ public class Veiculo implements Callable<Boolean>{
                 if(semaforos.vagaRegular().tryAcquire()){
                     estacionou = true;
                     logger.log(Level.INFO, "Veículo {0} estacionou na vaga regular.", name);
-                    Thread.sleep(random.nextInt(1000, 5000)); 
+                    Thread.sleep(tempoEstacionado = random.nextInt(1000, 5000)); 
                     semaforos.vagaRegular().release();
                 }else{
                     logger.log(Level.WARNING, "Veículo {0} não conseguiu estacionar na vaga regular.", name);
                 }
             }
 
+            logger.log(Level.INFO, "Veículo {0} ficou estacionado por {1} segundos", new Object[]{name, tempoEstacionado});
             semaforos.portaoSaida().acquire();
             logger.log(Level.INFO, "Veículo {0} passou pelo portão de saída.", name);
             Thread.sleep(100);
             semaforos.portaoSaida().release();
 
-            return estacionou;
+            return new veiculoDTO(estacionou, tempoEstacionado);
 
         }catch (InterruptedException e){
             logger.log(Level.SEVERE, "O veiculo {0} foi interrompido: {1}", new Object[]{name, e.getMessage()});
             Thread.currentThread().interrupt(); 
         }
 
-        return false;
+        return new veiculoDTO(false, 0);
     }
     
 }
